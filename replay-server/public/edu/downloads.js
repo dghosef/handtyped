5847967ff8c041fb463780758f2a59b0a14b990e
Joseph Tan <jdtan638@gmail.com>
2026-05-11 17:17:10 -0400

    Publish Windows key recapture student build

--- replay-server/public/edu/downloads.js
+++ replay-server/public/edu/downloads.js
@@ -1,4 +1,4 @@
-export const STUDENT_RELEASE_ID = '0.1.1-20260511.2038-windows-50ms-watchdog-signed'
+export const STUDENT_RELEASE_ID = '0.1.1-20260511.2108-windows-key-recapture-signed'
 
 export const STUDENT_DOWNLOADS = Object.freeze({
   macosAppleSilicon: Object.freeze({

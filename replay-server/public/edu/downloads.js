658d864c9980a1ec8beedade0b4850ec7e6e85a9
Joseph Tan <jdtan638@gmail.com>
2026-05-11 22:41:39 -0400

    Publish mac student cmd-q lockdown build

--- replay-server/public/edu/downloads.js
+++ replay-server/public/edu/downloads.js
@@ -1,4 +1,4 @@
-export const STUDENT_RELEASE_ID = '0.1.1-20260511.232436'
+export const STUDENT_RELEASE_ID = '0.1.1-20260512.000000-cmdq-lockdown'
 
 export const STUDENT_DOWNLOADS = Object.freeze({
   macosAppleSilicon: Object.freeze({
